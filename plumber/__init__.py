3d53c873196aa69d6a9200c4174f29b7e32ab7c7
Maharath Sinha <55414407+sin31415@users.noreply.github.com>
2024-10-08 15:22:01 -0400

    Delete plumber directory
    Plumber is controlled by sin31415 and exists at sin31415/plumber-cmc

--- plumber/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-
-from .MLClassifierPipeline import MLClassifierPipeline
-from .MLHyperTuning import MLHyperTuning
-
-# Optional aliasing for easier imports
-mlpipe = MLClassifierPipeline
-mlht = MLHyperTuning
